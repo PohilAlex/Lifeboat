e010f6d0dede178f1a10d4dbb9adad249b0642dc
Alex <av.pohil@gmail.com>
2017-09-26 22:28:35 +0300

    Changed icon and title

--- app/src/main/java/com/pokhyl/lifeboat/ui/MainActivity.java
+++ app/src/main/java/com/pokhyl/lifeboat/ui/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle(R.string.title_main_activity);
 
         initViews();
 
